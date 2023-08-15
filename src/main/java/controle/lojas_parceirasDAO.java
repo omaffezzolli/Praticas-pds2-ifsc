9a89638f6b26e816101d3562f733731f49a87bf7
omaffezzolli <joao.l09@aluno.ifsc.edu.br>
2023-08-15 08:27:21 -0300

    Bd

--- src/main/java/controle/lojas_parceirasDAO.java
+++ src/main/java/controle/lojas_parceirasDAO.java
@@ -3,10 +3,22 @@ package controle;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import modelo.lojas_parceiras;
 
 public class lojas_parceirasDAO {
+	
+	public ArrayList<lojas_parceiras> listar() {
+		Conexao c = Conexao.getInstancia();
+		
+		Connection con = c.conectar();
+		
+		ArrayList<lojas_parceiras> lojas = new ArrayList();
+		String query = "";
+		 return null;
+	}
+	
 	
 	public boolean inserir(lojas_parceiras lp) {
 		
